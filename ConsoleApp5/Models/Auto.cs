e51bf460ebc4200d54b3a55d17ebe7b4cee4873c
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-23 09:19:24 +0100

    Add project files.

---
+++ ConsoleApp5/Models/Auto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Models
+{
+    public class Auto
+    {
+        public int NumberDoors { get; set; }
+        public string Name { get; set; }
+    }
+}
